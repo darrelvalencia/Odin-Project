2beda062282b87944d31ef4a7cb82285f9a1ae67
darrelvalencia <149385642+darrelvalencia@users.noreply.github.com>
2023-11-01 20:20:22 +0800

    added playRound function that return and declares the winner

--- 03 rock-paper-scissors/javascript/javascript.js
+++ 03 rock-paper-scissors/javascript/javascript.js
@@ -6,3 +6,29 @@ function getComputerChoice () {
     return choice;
 }
 
+function playRound(playerSelection, computerSelection) {
+    
+    if (
+        (playerSelection == "rock" && computerSelection == "scissor") ||
+        (playerSelection == "paper" && computerSelection == "rock") ||
+        (playerSelection == "scissor" && computerSelection == "paper")
+        ) {
+            return `You Win! ${playerSelection} beats ${computerSelection}`
+    }
+
+    else if (
+        (playerSelection == "rock" && computerSelection == "paper") ||
+        (playerSelection == "paper" && computerSelection == "scissor") ||
+        (playerSelection == "scissor" && computerSelection == "rock")
+        ) {
+        return `You Lose! ${playerSelection} loses to ${computerSelection}`
+    }
+
+    else if (playerSelection == computerSelection) {
+        return `Draw! both uses ${playerSelection}`
+    }
+
+    else {
+        return "invalid input"
+    }
+}
