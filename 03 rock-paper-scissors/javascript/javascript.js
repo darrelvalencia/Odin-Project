17722279f4ae1f694f43c1137470b367b34f4128
darrelvalencia <149385642+darrelvalencia@users.noreply.github.com>
2023-11-02 17:44:31 +0800

    added game function that loops playRound 5 times with user prompt

--- 03 rock-paper-scissors/javascript/javascript.js
+++ 03 rock-paper-scissors/javascript/javascript.js
@@ -1,28 +1,27 @@
-console.log("Test");  
+console.log("Test");
 const options = ["rock", "paper", "scissor"];
 
-function getComputerChoice () {
+function getComputerChoice() {
     const choice = options[Math.floor(Math.random() * options.length)];
     return choice;
 }
 
 function playRound(playerSelection, computerSelection) {
-    
     const playerSelectionLowerCase = playerSelection.toLowerCase();
 
     if (
         (playerSelectionLowerCase == "rock" && computerSelection == "scissor") ||
         (playerSelectionLowerCase == "paper" && computerSelection == "rock") ||
         (playerSelectionLowerCase == "scissor" && computerSelection == "paper")
-        ) {
-            return `You Win! ${playerSelectionLowerCase} beats ${computerSelection}`
+    ) {
+        return `You Win! ${playerSelectionLowerCase} beats ${computerSelection}`
     }
 
     else if (
         (playerSelectionLowerCase == "rock" && computerSelection == "paper") ||
         (playerSelectionLowerCase == "paper" && computerSelection == "scissor") ||
         (playerSelectionLowerCase == "scissor" && computerSelection == "rock")
-        ) {
+    ) {
         return `You Lose! ${playerSelectionLowerCase} loses to ${computerSelection}`
     }
 
@@ -34,3 +33,18 @@ function playRound(playerSelection, computerSelection) {
         return "invalid input"
     }
 }
+
+// const playerSelection = "rock";
+// const computerSelection = getComputerChoice();
+// console.log(playRound(playerSelection, computerSelection));
+
+
+function game() {
+    for (let i = 0; i < 5; i++) {
+        const playerSelection = prompt("Rock Paper or Scissor?")
+        const computerSelection = getComputerChoice();
+        console.log(playRound(playerSelection, computerSelection));
+    }
+}
+
+game();
